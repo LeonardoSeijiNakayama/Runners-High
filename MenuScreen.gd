ea687c2594459d45694d9aa656cd6a1979421a3b
Leonardo <leonardonakayama99@gmail.com>
2026-01-27 03:42:15 -0400

    Changed Settings Scene

--- MenuScreen.gd
+++ MenuScreen.gd
@@ -17,6 +17,17 @@ onready var fireballAnimation3 = $Spatial/icosphere3/AnimationPlayer
 onready var fireballAnimation4 = $Spatial2/icosphere1/AnimationPlayer
 onready var fireballAnimation5 = $Spatial2/icosphere2/AnimationPlayer
 onready var fireballAnimation6 = $Spatial2/icosphere3/AnimationPlayer
+onready var menu = $Control/VBoxContainer
+
+onready var settings = $Control/Settings
+onready var settingsBackBtn = $Control/Settings/VBoxContainer/CenterContainer/btnBack
+onready var music_slider = $Control/Settings/VBoxContainer/HBoxContainer/VBoxContainer/SliderMVolume
+onready var sfx_slider = $Control/Settings/VBoxContainer/HBoxContainer/VBoxContainer/SliderGVolume
+
+const SAVE_PATH := "user://settings.cfg"
+
+var music_bus := -1
+var sfx_bus := -1
 
 var dir := {}
 
@@ -49,7 +60,6 @@ func _ready():
 
 func _setup_pingpong(p: AnimationPlayer) -> void:
 	dir[p] = 1
-	# conecta e passa o próprio player como argumento extra
 	p.connect("animation_finished", self, "_on_fireball_anim_finished", [p])
 
 
@@ -88,11 +98,72 @@ func _on_btnPlay_pressed():
 
 
 func _on_btnSettings_pressed():
-	set_process(false)
-	get_tree().change_scene("res://Settings.tscn")
+	music_bus = AudioServer.get_bus_index("Music")
+	sfx_bus = AudioServer.get_bus_index("SFX")
+	
+	var music_val = _load_value("Music", 70)
+	var sfx_val = _load_value("SFX", 70)
+	
+	music_slider.value = music_val
+	sfx_slider.value = sfx_val
+	
+	_apply_percent_to_bus(music_bus, music_val)
+	_apply_percent_to_bus(sfx_bus, sfx_val)
+	
+	settingsBackBtn.grab_focus()
+	
+	menu.visible = false
+	settings.visible = true
+
+
+func _on_btnBack_pressed():
+	menu.visible = true
+	settings.visible = false
+	btnSettings.grab_focus()
 
 
 func _on_btnExit_pressed():
 	get_tree().quit()
 
 
+func _on_SliderMVolume_value_changed(value):
+	_apply_percent_to_bus(music_bus, value)
+	_save_value("Music", value)
+
+func _on_SliderGVolume_value_changed(value):
+	_apply_percent_to_bus(sfx_bus, value)
+	_save_value("SFX", value)
+
+
+func _apply_percent_to_bus(bus_idx: int, percent: float) -> void:
+	var linear = percent / 100.0
+	
+	# Mute se estiver no zero
+	if linear <= 0.001:
+		AudioServer.set_bus_mute(bus_idx, true)
+		AudioServer.set_bus_volume_db(bus_idx, -80) # praticamente silêncio
+		return
+	
+	AudioServer.set_bus_mute(bus_idx, false)
+	
+	
+	var db = linear2db(linear)
+	
+	
+	db = clamp(db, -40, 0)
+	
+	AudioServer.set_bus_volume_db(bus_idx, db)
+
+func _save_value(key: String, value: float) -> void:
+	var cfg = ConfigFile.new()
+	cfg.load(SAVE_PATH) # se não existir, OK
+	cfg.set_value("audio", key, value)
+	cfg.save(SAVE_PATH)
+
+
+func _load_value(key: String, default_value: float) -> float:
+	var cfg = ConfigFile.new()
+	var err = cfg.load(SAVE_PATH)
+	if err != OK:
+		return default_value
+	return float(cfg.get_value("audio", key, default_value))
